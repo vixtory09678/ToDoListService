3ef13cb47f71718d175c2b7211bb1663dbd6b876
Wannachat Surasiang <wannachat.sur@wipay.co.th>
2022-01-25 18:20:13 +0700

    add decorator for check value is not be empty

--- src/users/dto/create_user.dto.ts
+++ src/users/dto/create_user.dto.ts
@@ -1,10 +1,12 @@
-import { IsString } from "class-validator";
+import { IsString, IsNotEmpty } from "class-validator";
 
 export class CreateUserDto {
-
+  
+  @IsNotEmpty()
   @IsString()
   username: string;
 
+  @IsNotEmpty()
   @IsString()
   password: string;
 }
