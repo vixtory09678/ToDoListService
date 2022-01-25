aca70f06b83f19f3d8a55b0e4a5c5884106c9f4c
Wannachat Surasiang <wannachat.sur@wipay.co.th>
2022-01-25 18:47:59 +0700

    add entity in user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/users.entity';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ UserEntity ])],
   providers: [UsersService]
 })
 export class UsersModule {}
