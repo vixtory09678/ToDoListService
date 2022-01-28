4c5ac73f7502068f408adc25f4ac13e9983d9d34
Wannachat Surasiang <wannachat.sur@wipay.co.th>
2022-01-28 09:41:11 +0700

    exports user service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,7 @@ import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ UserEntity ])],
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
