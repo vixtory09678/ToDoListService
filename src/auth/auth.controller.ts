a7f41f18e684a4c86f12b216da768291b48e2d28
Wannachat Surasiang <wannachat.sur@wipay.co.th>
2022-02-08 09:35:50 +0700

    blinding password with base64 encoded

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create_user.dto';
 import { UserDto } from 'src/users/dto/user.dto';
 import { LoginUserDto } from 'src/users/dto/user_login.dto';
@@ -11,11 +11,13 @@ export class AuthController {
 
   @Post('/signup')
   async signup(@Body() createUser: CreateUserDto) {
+    createUser.password = Buffer.from(createUser.password, 'base64').toString();
     return this.authService.signup(createUser);
   }
 
   @Post('/login')
   async login(@Body() loginUserDto: LoginUserDto) {
+    loginUserDto.password = Buffer.from(loginUserDto.password, 'base64').toString();
     return this.authService.login(loginUserDto);
   }
 
