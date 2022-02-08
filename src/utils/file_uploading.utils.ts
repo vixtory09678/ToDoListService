735785e1f6441cc8310b4775ffe9752b70612750
Wannachat Surasiang <wannachat.sur@wipay.co.th>
2022-02-08 09:38:13 +0700

    filter file typle

--- src/utils/file_uploading.utils.ts
+++ src/utils/file_uploading.utils.ts
@@ -10,7 +10,7 @@ export const editFileName = (req, file: Express.Multer.File, callback) => {
 };
 
 export const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+  if (!file.originalname.match(/\.(JPG|JPEG|jpg|jpeg|png|PNG|gif)$/)) {
     return callback(new Error('Only image files are allowed!'), false);
   }
   callback(null, true);
